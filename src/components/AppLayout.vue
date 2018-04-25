bebe9e8c311e261ddae220e1af199ce0371bc004
Pherang <pherang@users.noreply.github.com>
2018-04-24 20:59:45 -0400

    More layout files

---
+++ src/components/AppLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <header>
+    Votetastic
+    </header>
+
+
+    <!-- Poll Content -->
+    <router-vew />
+
+
+  </div>    
+</template>
+
+<script>
+export default {
+
+
+}
+</script>
+
+<style>
+</style>
