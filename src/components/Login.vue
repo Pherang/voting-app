7b0615c6249b0f16ac530c27a2300575d7866cdf
Pherang <pherang@users.noreply.github.com>
2018-05-04 23:13:41 -0400

    Adding input fields for login screen

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,19 +1,29 @@
 <template>
   <div>
-
     <!-- Login Main Content -->
-    Form for login goes here
-    UserName
-    Password
-    Login
+    <form @submit.prevent="submit">
+      <input type="text" name="username">UserName</input>
+      <input type="text" name="password">Password</input>
+      <button type="submit">Login</button>
+    </form>
   </div>    
 </template>
 
 <script>
 export default {
-
+  methods: {
+    async submit () {
+      console.log('Logging in...')
+    }
+  }
 }
 </script>
 
-<style>
+
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+  margin-top: 10%;
+}
 </style>
